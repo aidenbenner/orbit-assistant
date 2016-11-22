8f40e70d992edbb5c11f0acad956ab771b61cfa3
Lawrence Pan <lawrencefeipan@gmail.com>
2016-11-22 15:01:22 -0500

    literally + "\r\n" :stuck_out_tongue:

--- pi/c3po/utils.py
+++ pi/c3po/utils.py
@@ -6,4 +6,4 @@ def format_data(event_name, payload):
     dehydrate data into json string then encode to ascii
     """
     data = {'event': event_name, 'payload': payload}
-    return json.dumps(data).encode('ascii')
+    return json.dumps(data).encode('ascii') + '\r\n'
