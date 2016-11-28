c9b66cb3abdf6bcb47bdae7840de56f3536ac6ee
aidenbenner <aiden.benner@gmail.com>
2016-11-27 19:59:26 -0500

    fix news scroll bug

--- tiva/src/menu.cpp
+++ tiva/src/menu.cpp
@@ -47,42 +47,14 @@ static int curr_menu = 0;
 
 static long last_page_action_time = millis(); 
 
-//placeholder for testing 
-//this populates g_date and g_weather with dummy data
-
-const int NUM_REDDIT_POSTS = 5;
-struct Post reddit_news[NUM_REDDIT_POSTS];
-
-typedef struct Mail {
-  char * to;
-  char * from;
-  char * subject;
-  char * body;
-  bool read;
-  Date received;
-} Mail; 
 
 //will need to dynamically allocate this so we can have more messages
 int NUM_UNREAD_MAIL = 5; 
 const int NUM_MAIL_MESSAGES = 5; 
 Mail g_mail[NUM_MAIL_MESSAGES]; 
 
-//DUMMY DATA TEST ONLY
-char dummy_headline_1[] = "1. Edward Snowden's bid to guarantee that he would not be extradited to the US if he visited Norway has been rejected by the Norwedgian supreme court.";
-char dummy_headline_2[] = "2. Catholic Church Finally Apologizes for Its Role in the Deaths of Over 800K During Rwandan Genocide";
-char dummy_headline_3[] = "3. Top scientist who discovered Litvinenko poison 'stabbed himself to death with two knives' after trip to Russia";
-char dummy_headline_4[] = "4. Uganda is shutting down schools funded by Mark Zuckerberg, Bill Gates ";
-char dummy_headline_5[] = "5. Google is warning prominent journalists and professors that nation-sponsored hackers have recently targeted their accounts, according to reports delivered in the past 24 hours over social media";
-
 void test_data() 
 {
-
-  reddit_news[0].title = dummy_headline_1;
-  reddit_news[1].title = dummy_headline_2;
-  reddit_news[2].title = dummy_headline_3;
-  reddit_news[3].title = dummy_headline_4;
-  reddit_news[4].title = dummy_headline_5;
-
   g_mail[0].to = "aiden.benner@gmail.com";
   g_mail[0].from = "lpan@gmail.com";
   g_mail[0].subject = "Tiva project";
@@ -359,16 +331,16 @@ void news_page_tick (int selection)
   long init_time = millis(); 
   long time_selected_init = millis(); 
   long marquee_delay = 500; 
-
   int line_select = 1; 
   int page = 0 ; 
-  int page_max = g_jokes->number + 1;
-  int init_toggle = read_switch(0);;  
+  int page_max = g_jokes->number - 2;
+  int init_toggle = read_switch(0);  
   while(selection == get_menu_selection()){
     OrbitOledClearBuffer ();
     //page is article we start with
     //repeat for each line
     for(int i = 1; i<=3; i++){
+      Serial.println(page + i - 2);
       orbit_moveto_line(i);
       if(0 == page + i - 1){
         OrbitOledDrawString ("News: ");
@@ -381,7 +353,7 @@ void news_page_tick (int selection)
         marquee_text (g_jokes->posts[page +  i - 2].title, time_selected_init, marquee_delay); 
       }
       else{
-        OrbitOledDrawString (reddit_news[page + i - 2].title);
+        OrbitOledDrawString (g_jokes->posts[page + i - 2].title);
       }
     }
     oled_paint_progress_bar(page,page_max); 
@@ -389,7 +361,7 @@ void news_page_tick (int selection)
     menu_tick ();
     OrbitOledUpdate ();
     if(init_toggle != read_switch(0) ){
-      view_news_page (selection, reddit_news[page + line_select - 2]);
+      view_news_page (selection, g_jokes->posts[page + line_select - 2]);
     }
     int new_line_select = get_page_action (line_select, 4);
     if(new_line_select != line_select){
