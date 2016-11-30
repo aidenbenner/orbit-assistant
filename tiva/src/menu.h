00af33ec39778dac4916bdba76e948a88b14514d
Lawrence Pan <lawrencefeipan@gmail.com>
2016-11-30 17:42:01 -0500

    remove trailing white space + change unit to torr

--- tiva/src/menu.h
+++ tiva/src/menu.h
@@ -4,8 +4,8 @@
 extern long g_device_init_time;
 
 void menu_init ();
-void display_menu (); 
+void display_menu ();
 void display_long_string();
-void display_test (); 
+void display_test ();
 
-#endif 
+#endif
